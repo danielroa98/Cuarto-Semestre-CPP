05bf7a3b510c74eb68ef88012269be8081b55087
danielroa98 <daniel.roa98@gmail.com>
2019-04-01 13:07:01 -0600

    Tercer Parcial/Parcial Final

--- ListaLigada.cpp
+++ ListaLigada.cpp
@@ -9,41 +9,99 @@
  */
 
 #include <iostream>
+using namespace std;
 
-class Node{
+template<class T>
+class Node
+{
 public:
-    Node(){
+  Node()
+    { next = nullptr; }
 
-        next = nullptr;
+    //next = nullptr;
 
-    }
-    int value;
-    Node* next;
+    T value;
+    Node<T>* next;
 };
 
-void print(*Node node){
+template<class T>
+void printNormal(Node<T>* node){
 
+    Node<T>* temp = node;
 
+    while(temp != nullptr){
+        cout<< temp->value<<" ";
+        temp = temp -> next;
+
+    }
 
 }
 
-int main(){
+template<class T>
+void printRecursiva(Node<T>* node){
 
-    /*
-        Node n; //se crea en el stack (objeto concreto)
-        n.value = 6;
-     */
+    if(node == nullptr){
+        return;
+    }
 
-    Node* nodo = new Node();    //se crea en el heap
-    nodo -> value = 6;          //la flecha solo es usada para acceder el contenido del apuntador
-
-    //*node.value = 6;
-
-    Node* nodeEight = new Node();
-    nodeEight -> value = 8;
-    node -> next = nodeEight;
-
-    Node* nodeNine = new Node();
-    nodeNine -> value = 9;
-    nodeEight -> next(NodeNine);
+    else{
+        cout<<node->value<<" ";
+        return printRecursiva(node->next);
+    }
 }
+
+template<class T>
+class LinkedList{
+
+protected:
+    Node* root;
+
+public:
+    LinkedList(){
+
+        root = nullptr;
+
+    }
+
+    void add(Node<T>* node){
+
+        if(root == nullptr)
+            root = node;
+
+        else{
+            Node<T>* temp = traverse(root);
+            temp->next = node;
+        }
+    }
+
+    Node<T>* traverse(Node<T>* node){
+
+        if(node->next == nullptr)
+            return node;
+
+        else
+            return traverse(node -> next);
+
+    }
+};
+
+int main()
+{
+    //Node n; Esto se crea en el stack
+    //(*nodo).value = 6; Esto y lo que esta en la linea anterior son lo mismo
+
+  Node<char>* nodo = new Node<char>(); //Esto se crean en el heap
+  nodo -> value = George_;
+
+  Node<char>* nodeEight = new Node<char>();
+  nodeEight -> value = se_la_;
+  nodo -> next = nodeEight;
+
+  Node<char>* nodeNine = new Node<char>();
+  nodeNine -> value = come;
+  nodeEight -> next = nodeNine;
+
+  printRecursiva(nodo);
+
+  return 0;
+}//Close main
